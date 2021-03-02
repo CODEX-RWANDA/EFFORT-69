f0d3faa7b13326d2efd5efde73226f5a6d5bcafc
HabimanaSharif <hdidiersharif@gmail.com>
2021-03-02 22:37:06 +0200

    finishes setting up environment

--- src/app.js
+++ src/app.js
@@ -3,3 +3,18 @@ import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+
+import dbConnect from './database';
+import routes from './routes';
+
+const app = express();
+dbConnect();
+
+app.enable('trust proxy');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(routes);
+
+export default app;
